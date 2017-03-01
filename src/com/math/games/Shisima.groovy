1936d1b9cd8498a00d9c63b4959fe9f624c45adb
sachint147 <tiwarisachin147@gmail.com>
2017-03-01 23:33:28 +0530

    set up board

--- src/com/math/games/Shisima.groovy
+++ src/com/math/games/Shisima.groovy
@@ -7,10 +7,41 @@ package com.math.games
  * @author sachin
  *
  */
+
+class Node {
+	int nodeNumber;
+}
+
+class Board {
+	List<Node> nodes;
+}
+
+class Player {
+	int playerId;
+	String playerName;
+}
+
+
 class Shisima {
 
-	static main(args) {
+	static main(args) {		
+		Board board = initializeBoard();
+	}
 	
+	public Board initializeBoard() {
+		int numberOfNodes = 7;
+		Board board = new Board();
+		List<Node> nodes = new ArrayList<>();
+		for(int i=0; i<numberOfNodes; i++) {
+			Node node = new Node();
+			node.nodeNumber = i;
+			nodes.add(node);
+		}
+		board.nodes = nodes;
 	}
 
 }
+
+
+
+
