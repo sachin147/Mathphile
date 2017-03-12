a38e545ab5c4c0df8c1770f6e45d3e7c500da952
sachint147 <tiwarisachin147@gmail.com>
2017-03-12 23:02:42 +0530

    start stone movemnt

--- src/com/math/games/Shisima.groovy
+++ src/com/math/games/Shisima.groovy
@@ -10,16 +10,35 @@ package com.math.games
 
 class Node {
 	int nodeNumber;
+	boolean isEmpty;
+	Stone stone;
 }
 
 class Board {
 	List<Node> nodes;
+	
+	public List<Node> getNodes() {
+		return nodes;
+	}
+	
+	public void setNodes(List<Node> nodes) {
+		this.nodes = nodes;
+	}
+}
+
+class Stone {
+	String color;
+
+	public Stone(String color) {
+		super();
+		this.color = color;
+	}
 }
 
 class Player implements Runnable {
 	int playerId;
-	String playerName;
-		
+	String playerName;	
+	
 	public Player(int playerId, String playerName) {
 		super();
 		this.playerId = playerId;
@@ -29,31 +48,66 @@ class Player implements Runnable {
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		movePiece();
+		while(GameEngine.gameover) {			
+			GameEngine.play(this);
+			Thread.sleep(1000);
+		}
+		
 	}
 }
 
+class GameEngine {
+	public static boolean gameover;
+	
+	static synchronized void play(Player player) {
+		if(isShisimaEmpty()) {
+			
+		}
+	}
+	
+	static boolean isShisimaEmpty() {
+		//if(board node 0 stone absent)
+			
+	}
+}
 
 class Shisima {
 
 	static main(args) {		
 		Board board = initializeBoard();
 		Player player1 = new Player(1, "Sonu");
-		new Thread(player1).start();
+		new Thread(player1).start();		
 		Player player2 = new Player(2, "Monu");
 		new Thread(player2).start();
 	}
 	
-	public Board initializeBoard() {
-		int numberOfNodes = 7;
+	public static Board initializeBoard() {
+		int numberOfNodes = 9;
 		Board board = new Board();
 		List<Node> nodes = new ArrayList<>();
 		for(int i=0; i<numberOfNodes; i++) {
-			Node node = new Node();
-			node.nodeNumber = i;
-			nodes.add(node);
+			if(i==2 || i==3 || i==4) {	//bad code			
+				Node node = new Node();
+				node.nodeNumber = i;
+				node.isEmpty = true;
+				node.stone = new Stone('black');
+				nodes.add(node);
+			}
+			if(i==6 || i==7 || i==8) { //bad code
+				Node node = new Node();
+				node.nodeNumber = i;
+				node.isEmpty = true;
+				node.stone = new Stone('white');
+				nodes.add(node);
+			}
+			if(i==0 || i==1 || i==5) { //bad code
+				Node node = new Node();
+				node.nodeNumber = i;
+				node.isEmpty = true;
+				nodes.add(node);
+			}
 		}
-		board.nodes = nodes;
+		board.setNodes(nodes);
 	}
 
 }
